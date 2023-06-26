62ac011fd3cb84af3ccf5557b7a5a83648db0e4b
VAUBAN_TEMP\Administrateur <anastasiia.penkina@gmail.com>
2023-06-26 09:17:59 +0200

    upadtae page Connexion

--- beautyConnect/src/app/components/connexion/connexion.component.ts
+++ beautyConnect/src/app/components/connexion/connexion.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
@@ -6,9 +6,24 @@ import { NavbarComponent } from '../navbar/navbar.component';
   templateUrl: './connexion.component.html',
   styleUrls: ['./connexion.component.scss']
 })
-export class ConnexionComponent {
+export class ConnexionComponent implements OnInit {
   email: string | undefined;
   password: string | undefined;
 
-  
+  slides = [
+    { imageSrc: '../../../assets/pexels-ozan-çulha-15859572.jpg', altText: 'Image 1' },
+    { imageSrc: '../../../assets/pexels-bruno-silva-13931330.jpg', altText: 'Image 2' },
+    { imageSrc: '../../../assets/pexels-tokuo-nobuhiro-12717731.jpg', altText: 'Image 3' }
+  ];
+  currentSlideIndex = 0;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    setInterval(() => {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+    }, 3000);
+  }
 }
